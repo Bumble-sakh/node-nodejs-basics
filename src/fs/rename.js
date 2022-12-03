8b84eab6fd974ac383c801ce0b7e2a77960a4dcb
Bumble <bumble.sakh@gmail.com>
2022-12-03 14:29:24 +1100

    feat: add implementation rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import fs from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+  const oldPath = path.resolve(__dirname, 'files', 'wrongFilename.txt');
+  const newPath = path.resolve(__dirname, 'files', 'properFilename.md');
+
+  await fs
+    .access(newPath, fs.constants.F_OK)
+    .then(() => {
+      throw new Error('FS operation failed');
+    })
+    .catch((err) => {
+      if (err.message === 'FS operation failed') {
+        throw new Error('FS operation failed');
+      }
+      if (err.code === 'ENOENT') null;
+    });
+
+  await fs.rename(oldPath, newPath).catch((err) => {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  });
 };
 
 await rename();
