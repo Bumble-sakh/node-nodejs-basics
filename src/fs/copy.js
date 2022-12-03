a5c6c5e399f4e80dff41eee503f9ee1651c8c92a
Bumble <bumble.sakh@gmail.com>
2022-12-03 14:28:50 +1100

    feat: add implementation copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import fs from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const srcPath = path.resolve(__dirname, 'files');
+  const destPath = path.resolve(__dirname, 'files_copy');
+
+  await fs.opendir(srcPath).catch((err) => {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  });
+
+  await fs.cp(srcPath, destPath, { force: false, errorOnExist: true, recursive: true }).catch((err) => {
+    if (err.code === 'ERR_FS_CP_EEXIST') {
+      throw new Error('FS operation failed');
+    }
+  });
 };
 
 copy();
