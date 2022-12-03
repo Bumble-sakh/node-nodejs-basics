b74a325ffb9eadcc78111b73d11df61eb514e784
Bumble <bumble.sakh@gmail.com>
2022-12-03 14:28:35 +1100

    feat: add implementation args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.splice(2);
+
+  for (let i = 0; i < args.length; i++) {
+    const argument = args[i];
+    const value = args[i++];
+    console.log(`${argument} is ${value}`);
+  }
 };
 
 parseArgs();
