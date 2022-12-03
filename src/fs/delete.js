f1ef9cccbf7b090f97bd092c2826f8ae02539dcd
Bumble <bumble.sakh@gmail.com>
2022-12-03 14:29:07 +1100

    feat: add implementation delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, 'files', 'fileToRemove.txt');
+
+  await fs.rm(filePath).catch((err) => {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  });
 };
 
 await remove();
