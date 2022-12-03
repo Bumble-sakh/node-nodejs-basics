3bfc891b6ecd3e2cadf2ba7add1b8ac41dcd2e67
Bumble <bumble.sakh@gmail.com>
2022-12-03 14:28:41 +1100

    feat: add implementation env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+  const allEnv = process.env;
+  const rssEnv = Object.keys(allEnv).filter((key) => key.startsWith('RSS_'));
+  rssEnv.forEach((key) => console.log(`${key}=${allEnv[key]}`));
 };
 
 parseEnv();
