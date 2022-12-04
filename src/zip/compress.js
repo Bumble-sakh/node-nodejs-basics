3f8f25af63d48713bc5c85416e3cf592c182bdff
Bumble <bumble.sakh@gmail.com>
2022-12-04 12:36:32 +1100

    feat: add implementations compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import { createGzip } from 'zlib';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const inputPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const outputPath = path.join(__dirname, 'files', 'archive.gz');
+
+  const compress = createGzip();
+  const inputFile = fs.createReadStream(inputPath);
+  const outputFile = fs.createWriteStream(outputPath);
+
+  inputFile.pipe(compress).pipe(outputFile);
 };
 
 await compress();
