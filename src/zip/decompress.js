1ebf0fbd8bc60bb60ec0182f6232a408170d58b1
Bumble <bumble.sakh@gmail.com>
2022-12-04 12:36:38 +1100

    feat: add implementations decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import { createGunzip } from 'zlib';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const inputPath = path.join(__dirname, 'files', 'archive.gz');
+  const outputPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+  const decompress = createGunzip();
+
+  const inputFile = fs.createReadStream(inputPath);
+  const outputFile = fs.createWriteStream(outputPath);
+
+  inputFile.pipe(decompress).pipe(outputFile);
 };
 
 await decompress();
