558b8dba08ee877fd4c076816b2abcc887c0e4a8
Bumble <bumble.sakh@gmail.com>
2022-12-04 12:17:02 +1100

    feat: add implementation write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+  const file = await fs.open(filePath, 'w');
+  const ws = file.createWriteStream();
+
+  process.stdin.on('data', (data) => {
+    ws.write(data);
+  });
+
+  process.stdin.on('end', () => {
+    ws.close();
+    file.close();
+  });
 };
 
 await write();
