6c7939dcc2b9cc33a71c2064c4f35e86db4acb93
Bumble <bumble.sakh@gmail.com>
2022-12-04 12:16:53 +1100

    feat: add implementation read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, 'files', 'fileToRead.txt');
+  const file = await fs.open(filePath);
+  const rs = file.createReadStream();
+
+  rs.on('data', (data) => {
+    process.stdout.write(data.toString());
+  });
+
+  rs.on('end', () => {
+    rs.close();
+    file.close();
+  });
 };
 
 await read();
