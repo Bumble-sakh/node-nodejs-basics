0e3ea5d49359bc80a46636db7ac3feb8db248bf7
asessorov-iv <asessoroviv@intech.rshb.ru>
2025-10-25 15:41:57 +0300

    FEAT - env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-  // Write your code here
+  const rssVars = [];
+
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.startsWith("RSS_")) {
+      rssVars.push(`${key}=${value}`);
+    }
+  }
+
+  console.log(rssVars.join("; "));
 };
 
 parseEnv();
