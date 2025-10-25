7029abb3b10ff0a52b632fa6011eaa69e8a4b6d1
asessorov-iv <asessoroviv@intech.rshb.ru>
2025-10-25 15:39:39 +0300

    FEAT - delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { unlink } from "fs/promises";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FILE_PATH = join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-  // Write your code here
+  try {
+    await unlink(FILE_PATH);
+    console.log("File deleted successfully");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await remove();
