c1381c1881817fd1ca4ef563fb7f376908f7011d
asessorov-iv <asessoroviv@intech.rshb.ru>
2025-10-26 17:40:04 +0300

    FEAT - transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-  // Write your code here
+  console.log("Введите текст для переворота:");
+  console.log("(После ввода текста нажмите Enter для получения результата)");
+  console.log("(Для выхода из приложения нажмите Ctrl+C)");
+
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversed = chunk.toString().split("").reverse().join("") + "\n";
+      callback(null, reversed);
+      // После каждого преобразования выводим приглашение снова
+      console.log("\nВведите текст для переворота:");
+    },
+  });
+
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+  process.on("SIGINT", () => {
+    console.log("\n\nВыход из приложения...");
+    process.exit(0);
+  });
 };
 
 await transform();
